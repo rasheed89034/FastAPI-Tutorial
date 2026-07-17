5ea18b75cc3f7ccc62fcc712124de696768f2ff7
Rasheed Ahmad <rasheedahmad890127@gmail.com>
2026-07-17 07:10:01 +0500

    Update Post.py

--- CURD/Post.py
+++ CURD/Post.py
@@ -9,3 +9,25 @@ async def allUserData():
 @app.post("/user/create_user")
 async def create_new_user(new_user = Body()):
     data.append(new_user)
+
+@app.put("/update/updateuserData")
+async def update_user_data(updated_data = Body()):
+    for i in range(len(data)):
+        if data[i].get('name').casefold() == updated_data.get('name').casefold():
+            data[i] = updated_data
+            return {
+                "Message" : "Data Updated Successfully"
+            }
+    return {
+            "Message" : "User Not Found"
+        }
+
+@app.delete("/delete/deleteData")
+async def delete_user_data(name:str):
+    for i in range(len(data)):
+        if data[i].get('name').casefold() == name.casefold():
+            data.pop(i)
+            break
+            return "Data delete Successfully"
+
+
