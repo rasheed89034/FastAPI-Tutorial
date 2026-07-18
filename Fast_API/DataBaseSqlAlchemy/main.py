2bc5c7022af299a95aa33834eda107d0742acc3b
Rasheed Ahmad <rasheedahmad890127@gmail.com>
2026-07-18 13:02:11 +0500

    Implement user creation endpoint with database session
    Added database session management and user creation endpoint.

--- Fast_API/DataBaseSqlAlchemy/main.py
+++ Fast_API/DataBaseSqlAlchemy/main.py
@@ -1,8 +1,40 @@
-from fastapi import FastAPI
-from database import engine
+from fastapi import FastAPI,Depends,HTTPException,Path
+from database import engine,SessionLocal
 import models
+from pydantic import BaseModel,Field
+from models import Todos
+from typing import Annotated 
+from sqlalchemy.orm import Session
+from starlette import status
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+de_dependencies = Annotated[Session,Depends(get_db)]
+
+class TodoRequest(BaseModel):
+    name : str = Field(max_length=40)
+    address : str 
+    cgpa : float = Field(gt=0,lt=4.0)
+    isactive : bool
+
+
+@app.post("/createUser",status_code=status.HTTP_201_CREATED)
+def userData(db:de_dependencies,todo_request:TodoRequest):
+    todo_user = Todos(**todo_request.dict())
+
+    db.add(todo_user)
+    db.commit()
+
+
+
+
 
