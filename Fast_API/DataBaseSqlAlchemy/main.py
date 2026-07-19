e40a4eb59ce8322b1bde0733e7e87f7504a2ee99
Rasheed Ahmad <rasheedahmad890127@gmail.com>
2026-07-19 20:12:02 +0500

    Rename db dependency variable and add new routes

--- Fast_API/DataBaseSqlAlchemy/main.py
+++ Fast_API/DataBaseSqlAlchemy/main.py
@@ -18,7 +18,7 @@ def get_db():
     finally:
         db.close()
 
-de_dependencies = Annotated[Session,Depends(get_db)]
+db_dependencies = Annotated[Session,Depends(get_db)]
 
 class TodoRequest(BaseModel):
     name : str = Field(max_length=40)
@@ -28,11 +28,23 @@ class TodoRequest(BaseModel):
 
 
 @app.post("/createUser",status_code=status.HTTP_201_CREATED)
-def userData(db:de_dependencies,todo_request:TodoRequest):
+def userData(db:db_dependencies,todo_request:TodoRequest):
     todo_user = Todos(**todo_request.dict())
 
     db.add(todo_user)
     db.commit()
+
+@app.get("/",status_code=status.HTTP_200_OK)
+def read_all(db:db_dependencies):
+    return db.query(Todos).all()
+
+@app.get("/user/{user_id}",status_code=status.HTTP_200_OK)
+def getASpecificUser(db:db_dependencies,user_id:int=Path(gt=0)):
+    user = db.query(Todos).filter(Todos.id==user_id).first()
+
+    if user is not None:
+        return user
+    raise HTTPException(status_code=404,detail="User Not Found")
 
 
 
