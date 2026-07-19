26dcc18c4b0cd46f3611f2bc1c1e5b46a920a7e9
Rasheed Ahmad <rasheedahmad890127@gmail.com>
2026-07-19 20:23:13 +0500

    Add update and delete endpoints for todos

--- Fast_API/DataBaseSqlAlchemy/main.py
+++ Fast_API/DataBaseSqlAlchemy/main.py
@@ -47,6 +47,26 @@ def getASpecificUser(db:db_dependencies,user_id:int=Path(gt=0)):
     raise HTTPException(status_code=404,detail="User Not Found")
 
 
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependencies,todo_request : TodoRequest,todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id) .first()
+    if todo_model is None:
+        raise HTТРException(status_code=404, detail='Todo not found.')
+    
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+    db.add(todo_model) 
+    db.commit()
 
+@app.delete("/todo/{todo_id}",
+status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependencies, todo_id: int = Path(gt=0)) :
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HТТРЕxception(status_code=404, detail='Todo not found.')
+    db.query(Todos) .filter (Todos.id == todo_id). delete()
+    db.commit()
 
 
